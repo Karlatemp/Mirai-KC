3d90711aefa9ab5c7a50a5469418cdd82b91274b
Karlatemp <karlatemp@vip.qq.com>
2020-06-17 22:59:44 +0800

    Perm Generator

---
+++ mirai-kc/src/test/kotlin/perm/PermManager.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018-2020 Karlatemp. All rights reserved.
+ * @author Karlatemp <karlatemp@vip.qq.com> <https://github.com/Karlatemp>
+ * @create 2020/06/17 22:40:12
+ *
+ * Mirai-KC/Mirai-KC.mirai-kc.test/PermManager.kt
+ */
+
+package perm
+
+import io.karlatemp.github.mirai.permission.GroupNode
+import io.karlatemp.github.mirai.permission.PermNode
+import io.karlatemp.github.mirai.permission.PermissionManager
+import java.io.File
+import java.util.concurrent.ConcurrentHashMap
+
+fun main() {
+    val defaultManager = PermissionManager(ConcurrentHashMap(), ConcurrentHashMap())
+    defaultManager.findOrCreateUser(10086L)
+    defaultManager.groups["admin"] = GroupNode().also {
+        it.nodes.add(PermNode("command.*", true, mapOf()))
+    }
+    defaultManager.saveData(users = File("data/perm/users"), groups = File("data/perm/groups"))
+}
