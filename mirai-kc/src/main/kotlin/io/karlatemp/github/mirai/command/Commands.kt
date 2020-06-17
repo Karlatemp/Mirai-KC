363bb2db7112f40d1794df282363ed04d723741e
Karlatemp <karlatemp@vip.qq.com>
2020-06-17 22:58:25 +0800

    Add functions

--- mirai-kc/src/main/kotlin/io/karlatemp/github/mirai/command/Commands.kt
+++ mirai-kc/src/main/kotlin/io/karlatemp/github/mirai/command/Commands.kt
@@ -18,10 +18,10 @@ annotation class KCommand(val value: String)
 
 interface Command {
     suspend operator fun invoke(
-            sender: User,
-            subject: Contact,
-            message: MessageEvent,
-            parsed: LinkedList<ArgumentToken>
+        sender: User,
+        subject: Contact,
+        message: MessageEvent,
+        parsed: LinkedList<ArgumentToken>
     )
 
     val permission: String? get() = null
@@ -31,3 +31,48 @@ object Commands {
     val commands = HashMap<String, Command>()
 }
 
+open class BaseCommand(
+    override val permission: String? = null,
+    private val invoke: suspend (
+        sender: User,
+        subject: Contact,
+        message: MessageEvent,
+        parsed: LinkedList<ArgumentToken>
+    ) -> Unit
+) : Command {
+    override suspend fun invoke(
+        sender: User,
+        subject: Contact,
+        message: MessageEvent,
+        parsed: LinkedList<ArgumentToken>
+    ) {
+        this.invoke.invoke(sender, subject, message, parsed)
+    }
+}
+
+fun registerCommand(
+    name: String,
+    permission: String? = null,
+    invoke: suspend (
+        sender: User,
+        subject: Contact,
+        message: MessageEvent,
+        parsed: LinkedList<ArgumentToken>
+    ) -> Unit
+) {
+    registerCommand(name, newCommand(permission, invoke))
+}
+
+fun registerCommand(name: String, command: Command) {
+    Commands.commands[name.toLowerCase()] = command
+}
+
+fun newCommand(
+    permission: String? = null,
+    invoke: suspend (
+        sender: User,
+        subject: Contact,
+        message: MessageEvent,
+        parsed: LinkedList<ArgumentToken>
+    ) -> Unit
+) = BaseCommand(permission, invoke)
