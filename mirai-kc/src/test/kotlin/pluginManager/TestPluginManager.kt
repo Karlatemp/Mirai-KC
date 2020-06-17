7a9668f98e5196a6ee386564da5f80cd050bd74e
Karlatemp <karlatemp@vip.qq.com>
2020-06-17 22:59:32 +0800

    Plugin Manager Tester

---
+++ mirai-kc/src/test/kotlin/pluginManager/TestPluginManager.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018-2020 Karlatemp. All rights reserved.
+ * @author Karlatemp <karlatemp@vip.qq.com> <https://github.com/Karlatemp>
+ * @create 2020/06/17 22:51:32
+ *
+ * Mirai-KC/Mirai-KC.mirai-kc.test/TestPluginManager.kt
+ */
+
+package pluginManager
+
+import io.karlatemp.github.mirai.plugin.PluginManager
+import kotlin.system.exitProcess
+
+fun main() {
+    PluginManager.reload()
+    Thread.sleep(5000)
+    PluginManager.reload()
+    Thread.sleep(5000)
+    exitProcess(0)
+}
