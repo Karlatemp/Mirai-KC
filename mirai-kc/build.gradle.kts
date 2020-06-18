9eb386c0c37c01139fd9aa42b2489bea892a6da1
Karlatemp <karlatemp@vip.qq.com>
2020-06-18 23:45:26 +0800

    Shadow Jar

--- mirai-kc/build.gradle.kts
+++ mirai-kc/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "1.3.72"
     id("com.github.johnrengelman.shadow") version "5.2.0"
@@ -47,3 +49,38 @@ tasks {
         kotlinOptions.jvmTarget = "1.8"
     }
 }
+
+tasks.withType(ShadowJar::class.java) {
+    this.archiveFileName.set("Mirai-KC-${project.version}.jar")
+    dependencies {
+        exclude {
+            when ("${it.moduleGroup}:${it.moduleName}") {
+                "net.mamoe:mirai-core" -> true
+                "net.mamoe:mirai-core-qqandroid" -> true
+                "moe.him188:jcekt" -> true
+                "moe.him188:jcekt-common" -> true
+                "net.mamoe:mirai" -> true
+                "org.bouncycastle:bcprov-jdk15on" -> true
+                else -> {
+                    when (it.moduleGroup) {
+                        "io.ktor" -> true
+                        else -> {
+                            println("${it.moduleGroup}:${it.moduleName} ${it.moduleVersion}")
+                            false
+                        }
+                    }
+                }
+            }
+        }
+    }
+    manifest {
+        attributes(
+            "Manifest-Version" to "1.0",
+            "Implementation-Vendor" to "Karlatemp <karlatemp@vip.qq.com>",
+            "Implementation-Title" to project.name.toString(),
+            "Implementation-Version" to project.version.toString(),
+            "Main-Class" to "io.karlatemp.github.mirai.bootstrap.Main"
+        )
+    }
+}
+apply(from = project.file("gradle/shadowjar.gradle"))
